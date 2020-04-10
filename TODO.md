449dcd432f305281f2777a546c2a53e29472d122
Annisa Feryannie <aferyannie@gmail.com>
2020-04-10 20:27:38 +0700

    chore(webpack): extend plugins

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+- [x] Transpile ES6 to ES5 (or older)
+- [x] Add live-reloading (dev-server)
+- [ ] Handle styling (CSS/SCSS)
+- [ ] Handle image and other static image
+- [ ] Separate production and development 
+- [ ] Add source map
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: 'localhost',
+    port: 9000,
+  },
   output: {
-    // path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
